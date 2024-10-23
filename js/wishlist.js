91750e6fd549e00227058d303cb12192a3f10f46
joy935 <tupaiv@gmail.com>
2024-10-23 16:32:59 +0200

    displayed wishlist

--- js/wishlist.js
+++ js/wishlist.js
@@ -23,6 +23,27 @@ function displayWishlist(wishlistItems) {
     const bookList = document.querySelector(".bookList");
 
     wishlistItems.forEach(async (book) => {
-        // define
-    });
+        const apiUrl = "https://www.googleapis.com";
+        const key = "AIzaSyD0ESRed2KpWg351u_7MGA70O2jZIgvXb4";  
+
+        const response = await fetch(`${apiUrl}/books/v1/volumes/${book}?key=${key}`);
+        const data = await response.json();
+
+        const image = data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : "../images/no-image.jpg"
+        const title = data.volumeInfo.title || "No title available";
+        const authors = data.volumeInfo.authors || "No author available";
+        const publisher = data.volumeInfo.publisher || "No publisher available";
+
+        return bookList.innerHTML +=`
+            <div class="book">
+                <a href="../book/index.html?id=${book}">
+                    <img class="thumbnail" src="${image}" alt="${title}" width="128" height="179">
+                    <h2>${title}</h2>
+                </a>
+                <p class="author">${authors}</p>
+                <p class="publisher">Publisher: ${publisher}</p>
+            </div>
+        `;
+
+    })
 }
