4b6c8f4ddd5d14213bd0b8ed4624ea85b0f1a809
joy935 <tupaiv@gmail.com>
2024-10-24 12:59:12 +0200

    fixed inspire page

--- js/inspire.js
+++ js/inspire.js
@@ -2,19 +2,21 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const apiUrl = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json";
+const apiUrl = "https://api.nytimes.com/svc/books/v3/lists.json";
 const key = "pDrDjUpIh4tcpfr713QffVM0OhPgJFuX";
 const books = document.querySelector(".bookList");
-getInspirtation();
+const books2 = document.querySelector(".bookList2");
+// get the best sellers
+fetchAndRenderBooks("hardcover-fiction", books);
+fetchAndRenderBooks("hardcover-nonfiction", books2);
 
-async function getInspirtation() {
+async function fetchAndRenderBooks(listType, element) {
     try {
-        const response = await fetch(`${apiUrl}?api-key=${key}`);
+        const response = await fetch(`${apiUrl}?list=${listType}&api-key=${key}`);
         const data = await response.json();
-        const filteredData = data.results.slice(0, 6); // add filter to get only 6 books
-        await renderInspiration(filteredData);
+        await renderBooks(data, element);
     } catch (error) {
-        console.error(error);
+        console.error("Error fetching ${listType} ", error);
     }
 }
 
@@ -56,16 +58,21 @@ async function getBookDetails(isbn) {
     }
 }
     
-async function renderInspiration(data) {
+async function renderBooks(data, element) {
+    const limitedResults = data.results.slice(0, 4); // add filter to get only 4 books
     const html = await Promise.all(
-        data.map(async (book) => {
+        limitedResults.map(async (book) => {
+
+            const bookDetails = book.book_details[0];
+
             const isbn = book.isbns.length > 0 ? book.isbns[0].isbn13 : null; // get the first isbn13
             const bookImage = isbn ? await getBookCover(isbn) : "../images/no-image.jpg"; // get the book cover
             const bookId = isbn ? await getBookDetails(isbn) : "No id available"; // get the book id
 
             // if one of the properties is not available, display a default value
-            const title = book.title || "No title available";
-            const author = book.author || "No author available";
+            const title = bookDetails.title || "No title available";
+            const author = bookDetails.author || "No author available";
+            const description = bookDetails.description || "No description available";
 
             // change to lowercase
             const lowerCaseTitle = title.toLowerCase().trim();
@@ -90,10 +97,11 @@ async function renderInspiration(data) {
                     <img class="thumbnail" src="${bookImage}" alt="${book.title}" width="128" height="179">
                     <h2>${capitalizedTitle}</h2>
                     </a>
-                    <p>${author}</p>
+                    <p class="author">${author}</p>
+                    <p>${description}</p>
                 </div>
             `;
         })
     );
-    books.innerHTML = html.join("");
+    element.innerHTML = html.join("");
 }
