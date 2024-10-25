0d76ddf636bcb6c292a8015f2a17fe1b6ce434ea
joy935 <tupaiv@gmail.com>
2024-10-25 22:03:26 +0200

    created dynamic navigation

---
+++ js/dynNav.js
@@ -0,0 +1,18 @@
+let dynamicNav = document.getElementById("LoginLink");
+let isLoggedIn = localStorage.getItem("isLoggedIn");
+let isRegistered = localStorage.getItem("isRegistered");
+let users = JSON.parse(localStorage.getItem("users"));
+
+if (isLoggedIn) {
+    dynamicNav.innerHTML = "My Account"
+    dynamicNav.href = "../account/index.html"
+} else if (users) {
+    dynamicNav.innerHTML = "Log In"
+    dynamicNav.href = "../login/index.html"
+} else if (isRegistered) {
+    dynamicNav.innerHTML = "Log In"
+    dynamicNav.href = "../login/index.html"
+} else {
+    dynamicNav.innerHTML = "Register"
+    dynamicNav.href = "../register/index.html"
+}
